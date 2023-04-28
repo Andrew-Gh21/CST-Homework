93636f30f6928149aaf37e27fc23efa859947148
Andrei Gheorghita <gheorghita.andrei001@gmail.com>
2023-04-28 19:06:56 +0300

    Deleted bad folder

--- Tema2/tema2/src/app/data-grid/pipes/header-column-names.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HeaderColumnNamesPipe } from './header-column-names.pipe';
-
-describe('HeaderColumnNamesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new HeaderColumnNamesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- Tema2/tema2/src/app/interfaces/person.ts
+++
@@ -1,9 +0,0 @@
-export interface Person {
-  Id:string,
-  FirstName:string,	
-  LastName:string,
-  Address:string,
-  Gender:string,	
-  RegistrationDate: string;
-  LastModified: string;
-}
